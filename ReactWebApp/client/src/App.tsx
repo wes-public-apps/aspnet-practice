9d74b0e087496daec136b004716eed935f5fdd42
wes-public-apps <murraywj@yahoo.com>
2021-03-01 12:41:46 -0800

    add support for login through microsoft

--- ReactWebApp/client/src/App.tsx
+++ ReactWebApp/client/src/App.tsx
@@ -1,11 +1,49 @@
-
-import React from 'react';
-import Chat from './Chat';
+import React, { useState } from "react";
+import AzureAuthenticationButton from "./azure/azure-authentication-component";
+import { AccountInfo } from "@azure/msal-browser";
+import Chat from "./Chat";
 
 function App() {
+  // current authenticated user
+  const [currentUser, setCurrentUser] = useState<AccountInfo>();
+
+  // authentication callback
+  const onAuthenticated = async (userAccountInfo: AccountInfo) => {
+    setCurrentUser(userAccountInfo);
+  };
+
+  // Render JSON data in readable format
+  const PrettyPrintJson = ({ data }: any) => {
+    return (
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    );
+  };
+
+  // Quick link - user revokes app's permission
+  const ShowPermissionRevokeLinks = () => {
+    return (
+      <div>
+        <div><a href="https://myapps.microsoft.com" target="_blank" rel="noopener">Revoke AAD permission</a></div>
+        <div><a href="https://account.live.com/consent/manage" target="_blank" rel="noopener">Revoke Consumer permission</a></div>
+      </div>
+    );
+  };
+
   return (
-    <div style={{ margin: '0 30%' }}>
-      <Chat />
+    <div id="App">
+      <h2>Microsoft Login Button application</h2>
+      <AzureAuthenticationButton onAuthenticated={onAuthenticated} />
+      {currentUser && (
+        <>
+        <div>
+          <PrettyPrintJson data={currentUser} />
+          <ShowPermissionRevokeLinks />
+        </div>
+        <Chat />
+        </>
+      )}
     </div>
   );
 }
