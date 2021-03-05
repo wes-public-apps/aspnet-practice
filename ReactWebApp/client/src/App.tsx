51a9e380576d4a8139dd36f699a9e544027298ee
wes-public-apps <murraywj@yahoo.com>
2021-03-05 09:20:50 -0500

    Revert "broken login using Azure Active Directory"
    This reverts commit e6ac38b3baeb6f3ab9c6ebb502a21b3e4798158a.

--- ReactWebApp/client/src/App.tsx
+++ ReactWebApp/client/src/App.tsx
@@ -1,42 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { Container } from 'reactstrap';
-import NavBar from './NavBar';
-import ErrorMessage from './ErrorMessage';
-import 'bootstrap/dist/css/bootstrap.css';
-import Home from './Home';
-import withAuthProvider, { AuthComponentProps } from './AuthProvider';
 
-class App extends Component<AuthComponentProps> {
-  render() {
-    let error = null;
-    if (this.props.error) {
-      error = <ErrorMessage
-        message={this.props.error.message}
-        debug={this.props.error.debug} />;
-    }
+import React from 'react';
+import Chat from './Chat';
 
-    return (
-      <Router>
-        <div>
-          <NavBar
-            isAuthenticated={this.props.isAuthenticated}
-            authButtonMethod={this.props.isAuthenticated ? this.props.logout : this.props.login}
-            user={this.props.user} />
-          <Container>
-            {error}
-            <Route exact path="/"
-              render={(props) =>
-                <Home {...props}
-                  isAuthenticated={this.props.isAuthenticated}
-                  user={this.props.user}
-                  authButtonMethod={this.props.login} />
-              } />
-          </Container>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div style={{ margin: '0 30%' }}>
+      <Chat />
+    </div>
+  );
 }
 
-export default withAuthProvider(App);
+export default App;
